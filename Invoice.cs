7d2077ca35c7e7998b4e85325f1fd8523aa0982f
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-08 00:58:51 +0300

    Update Invoice.cs

--- Invoice.cs
+++ Invoice.cs
@@ -7,6 +7,12 @@ public class Invoice
 
     public Invoice(DateTime issueDate) 
     {
+
+            if(issueDate == null)
+            {
+                throw new ArgumentNullException( "Дата выставления счёта не может быть null :( ");
+            }
+        
         if(issueDate.Date < DateTime.Today)
         {
             throw new InvoiceDateException("Дата выставления счёта уже прошла!");
